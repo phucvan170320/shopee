660911aeae95026663821c7e356a779c1dc85bce
phucdev <phucvan.pix@gmail.com>
2023-01-10 23:08:31 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
 import useRouteElements from './useRouteElements'
-
+import { ToastContainer } from 'react-toastify'
 function App() {
   const routeElements = useRouteElements()
   return (
     <>
-      <div className=''>{routeElements}</div>
+      <div className=''>
+        {routeElements}
+        <ToastContainer />
+      </div>
     </>
   )
 }
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,13 @@
-import axios, { type AxiosInstance } from 'axios'
+import axios, { AxiosError, type AxiosInstance } from 'axios'
+import HttpStatusCode from 'axios'
+import { isAxiosUnprocessableEntityError } from './ultils'
+import { toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
   constructor() {
     this.instance = axios.create({
-      baseURL: 'https://api-ecom.duthanhduoc.com/',
+      baseURL: 'https://api-ecom.duthanhduoc.com1/',
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
@@ -12,6 +15,20 @@ class Http {
         'expire-refresh-token': 60 * 60 * 24 * 160 // 160 ngày
       }
     })
+
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== HttpStatusCode.HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data?.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 const http = new Http().instance
