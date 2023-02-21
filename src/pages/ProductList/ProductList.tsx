60a0e5e48adde12038b102c05248adced4bf7869
phucdev <phucvan.pix@gmail.com>
2023-02-21 10:06:59 +0700

    update

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -5,37 +5,45 @@ import Product from './components/Product'
 import { useQuery } from '@tanstack/react-query'
 import useQueryParams from '../../hooks/useQueryParams'
 import productApi from '../../apis/product.api'
+import Pagination from '../../components/Pagination'
 // import Product from '../../types/product.type'
+import { useState } from 'react'
+import useQueryConfig from '../../hooks/useQueryConfig'
+import { ProductListConfig } from '../../types/product.type'
 
 function ProductList() {
-  const queryParams = useQueryParams()
+  const queryConfig = useQueryConfig()
+
+  // const queryParams = useQueryParams()
   const { data } = useQuery({
-    queryKey: ['products', queryParams],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
-      return productApi.getProducts(queryParams)
+      return productApi.getProducts(queryConfig as ProductListConfig)
     }
   })
   console.log('datta:', data)
+  // const [page, setPage] = useState(1)
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container '>
-        <div className='grid grid-cols-12 gap-6'>
-          <div className='col-span-3'>
-            <AsideFilter />
-          </div>
-          <div className='col-span-9'>
-            <SortProductList />
-            <div className=' mt-6 grid grid-cols-2 gap-3  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
+        {data && (
+          <div className='grid grid-cols-12 gap-6'>
+            <div className='col-span-3'>
+              <AsideFilter />
+            </div>
+            <div className='col-span-9'>
+              <SortProductList />
+              <div className=' mt-6 grid grid-cols-2 gap-3  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                {data.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
                   </div>
                 ))}
+              </div>
+              <Pagination queryConfig={queryConfig} pageSize={data.data.data.pagination.page_size} />
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
--- src/pages/ProductList/components/Product/Product.tsx
+++ src/pages/ProductList/components/Product/Product.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 // eslint-disable-next-line import/no-unresolved
 import { Product as ProductType } from '../../../../types/product.type'
 import ProductRating from '../../../../components/ProductRating/ProductRating'
+import { formatCurrency, formatNumberToSocialStyle } from '../../../../utils/ultils'
 interface Props {
   product: ProductType
 }
@@ -12,29 +13,27 @@ function Product({ product }: Props) {
       <div className='overflow-hidden rounded-sm bg-white shadow transition-transform duration-100 hover:translate-y-[-0.04rem] hover:shadow-md'>
         <div className='relative w-full pt-[100%]'>
           <img
-            src={'https://cf.shopee.vn/file/f774bd6b10710f86b8cecf6c0f447ea8_tn'}
-            alt=''
+            src={product.image}
+            alt={product.name}
             className='absolute top-0 left-0 h-full w-full bg-white object-cover'
           />
         </div>
         <div className='overflow-hidden p-2'>
-          <div className='min-h-[2rem] text-xs line-clamp-2'>
-            Bộ Quần Áo Thun Nam In Họa Tiết Chữ UMKLSU Tinh Tế Thời Trang Zenkonam MEN QA 129
-          </div>
+          <div className='min-h-[2rem] text-xs line-clamp-2'>{product.name}</div>
           <div className='mt-3 flex items-center'>
             <div className='max-w-[50%] truncate text-gray-500 line-through'>
               <span className='text-xs'>₫</span>
-              <span className='text-sm'>5000</span>
+              <span className='text-sm'>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='ml-1 truncate text-orange'>
               <span className='text-xs'>₫</span>
-              <span className='text-sm'>2000</span>
+              <span className='text-sm'>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <ProductRating rating={4} />
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
-              <span>5,66k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
