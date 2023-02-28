961a77bd8b964674b9b069547977863085f4820d
phucdev <phucvan.pix@gmail.com>
2023-02-28 15:16:09 +0700

    update file

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes, forwardRef } from 'react'
+import React, { InputHTMLAttributes, forwardRef, useState } from 'react'
 import { ref } from 'yup'
 
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -15,7 +15,7 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     className,
     errorMessage,
     onChange,
-
+    value = '',
     // autoComplete,
     classNameError = 'mt-[0.5rem] min-h-[1.25rem] text-sm font-[900] text-red-900 ',
     classNameInput = 'w-full rounded-sm border border-gray-300  p-1 outline-none focus:border-gray-500 focus:shadow-sm',
@@ -23,13 +23,14 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
   },
   ref
 ) {
+  const [localValue, setLocalValue] = useState<string>(value as string)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
     if (/^\d+$/.test(value) || value === '') {
       // Thực thi onChange callback từ bên ngoài truyền vào props
       onChange && onChange(event)
       // Cập nhật localValue state
-      // setLocalValue(value)
+      setLocalValue(value)
     }
   }
   return (
@@ -41,6 +42,7 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
         className={classNameInput}
         {...rest}
         ref={ref}
+        value={value || localValue}
       />
       <div className={classNameError}>{errorMessage}</div>
     </div>
